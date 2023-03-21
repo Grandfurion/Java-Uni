3d9a204b62b727dc30d5e9763b5bc969e62efafa
grandfurion <jarikpar@gmail.com>
2023-03-21 20:11:35 +0200

    Created Git Repository. Commited Lab#1 task

---
+++ Lab0/Main.java
@@ -0,0 +1,8 @@
+package edu.nmu.Lab0;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Lab #1\nTask: Print 'Hello World'\n" +
+                "Execution: Hello World!");
+    }
+}
